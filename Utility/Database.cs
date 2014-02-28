515322a7ca2a0bcc1cc4e5ce2b3727bff750ccc8
Carlos Gomes <cgomes@iinet.com>
2014-02-28 14:53:42 -0800

    More functions

---
+++ Utility/Database.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Utility
+{
+    class Database
+    {
+        public static void ExecSqlScript(string query)
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]))
+            {
+                var sqlBatch = string.Empty;
+                var cmd = new SqlCommand(string.Empty, conn);
+                conn.Open();
+                query += "\nGO";   // make sure last batch is executed.
+
+                var startLineNumber = 1;
+                var endLineNumber = 0;
+
+                try
+                {
+                    foreach (var line in query.Split(new string[2] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (line.ToUpperInvariant().Trim() == "GO")
+                        {
+                            if (!string.IsNullOrEmpty(sqlBatch.Trim()))
+                            {
+                                Console.WriteLine("Executing:\n" + sqlBatch);
+                                cmd.CommandText = sqlBatch;
+                                cmd.ExecuteNonQuery();
+                                sqlBatch = string.Empty;
+                            }
+
+                            endLineNumber++;
+                            startLineNumber = endLineNumber;
+                        }
+                        else
+                        {
+                            sqlBatch += line + "\n";
+                            endLineNumber++;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(string.Format("Execute query script lines( {0},{1} ) Error: {2}", endLineNumber, startLineNumber, sqlBatch), ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
--- Utility/Utility.cs
+++ Utility/Utility.cs
@@ -1,57 +1,56 @@
 ﻿using System;
-using System.Configuration;
-using System.Data.SqlClient;
+using System.IO;
 
 namespace Utility
 {
     public class Utility
     {
-        public static void ExecSqlScript(string query)
+        public static string FindAssemblyFile(string filePath)
         {
-            using (var conn = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]))
-            {
-                var sqlBatch = string.Empty;
-                var cmd = new SqlCommand(string.Empty, conn);
-                conn.Open();
-                query += "\nGO";   // make sure last batch is executed.
+            if (File.Exists(filePath))
+                return filePath;
 
-                var startLineNumber = 1;
-                var endLineNumber = 0;
+            var fileName = Path.GetFileName(filePath);
+            if (File.Exists(fileName))
+                return fileName;
 
-                try
-                {
-                    foreach (var line in query.Split(new string[2] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries))
-                    {
-                        if (line.ToUpperInvariant().Trim() == "GO")
-                        {
-                            if (!string.IsNullOrEmpty(sqlBatch.Trim()))
-                            {
-                                Console.WriteLine("Executing:\n" + sqlBatch);
-                                cmd.CommandText = sqlBatch;
-                                cmd.ExecuteNonQuery();
-                                sqlBatch = string.Empty;
-                            }
+            var folder = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var checkPath = Path.Combine(folder, filePath);
+            if (File.Exists(checkPath))
+                return checkPath;
 
-                            endLineNumber++;
-                            startLineNumber = endLineNumber;
-                        }
-                        else
-                        {
-                            sqlBatch += line + "\n";
-                            endLineNumber++;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(string.Format("Execute query script lines( {0},{1} ) Error: {2}", endLineNumber, startLineNumber, sqlBatch), ex);
-                }
-                finally
-                {
-                    conn.Close();
-                }
-            }
+            checkPath = Path.Combine(folder, fileName);
+            if (File.Exists(checkPath))
+                return checkPath;
+
+            folder = System.Reflection.Assembly.GetCallingAssembly().Location;
+            checkPath = Path.Combine(folder, fileName);
+            if (File.Exists(checkPath))
+                return checkPath;
+
+            checkPath = Path.Combine(folder, fileName);
+            if (File.Exists(checkPath))
+                return checkPath;
+
+            return null;
+            //throw new Exception(string.Format("Missing {0}", filePath));
         }
 
+        /// <summary>
+        /// Loads a resource from file if found, or from embedded resource
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="resourceLookupFunction"></param>
+        /// <returns></returns>
+        public static string LoadResource(string filePath, Func<string> resourceLookupFunction)
+        {
+            var foundFile = Utility.FindAssemblyFile(filePath);
+            if (!string.IsNullOrEmpty(foundFile))
+            {
+                return File.ReadAllText(foundFile);
+            }
+
+            return resourceLookupFunction();
+        }
     }
 }
